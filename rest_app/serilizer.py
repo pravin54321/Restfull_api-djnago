a690d6972f84ec136b0e6efbae5c19070d3b5a19
pravin <mendhe.pravin123@gmail.com>
2022-10-10 15:39:52 +0530

    serilizer changes

--- rest_app/serilizer.py
+++ rest_app/serilizer.py
@@ -4,4 +4,6 @@ from .models  import *
 class student_serilizer(serializers.ModelSerializer):
     class Meta:
         model=Student
-        fields='__all__'
+        #fields='__all__'
+        #fields=['Naame','Age']
+        exclude=['id']
